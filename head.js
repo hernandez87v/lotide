7ca66238505ba379d00f281a590c921e7b489ce0
Vlad <hernandez87v@hotmail.com>
2020-03-03 02:57:50 +0000

    Adding Implement head

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁😁Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😞😞Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(assertEqual) {
+  console.log(assertEqual[0]);
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
