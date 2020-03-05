d49d52466c16f70a1875ae2849c48b84310add70
Vlad <hernandez87v@hotmail.com>
2020-03-05 03:46:57 +0000

    Adding Assertion Array assignment

---
+++ assertArrayEqual.js
@@ -0,0 +1,20 @@
+let assertEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(`😞😞Assertion Failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`😞😞Assertion Failed: ${arr1} !== ${arr2}`);
+      return false;
+    }
+  }
+  console.log(`😁😁Assertion Passed: ${arr1} === ${arr2}`);
+  return true;
+};
+
+console.log(assertEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertEqual([1, 2, 3], [1, 2, 3, 4]));
+console.log(assertEqual([1, 2, 3], [3, 2, 1, 4])); // => false
+console.log(assertEqual(['1', '2', '3'], ['1', '2', '3'])); // => true
+console.log(assertEqual(['1', '2', '3'], ['1', '2', 3])); // => false
