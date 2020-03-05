ebef690b3d9396ff28ea800e92573459bc5e6616
Vlad <hernandez87v@hotmail.com>
2020-03-05 04:09:18 +0000

    renamed function so its different the eqArray

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,4 +1,4 @@
-let assertEqual = function(arr1, arr2) {
+let assertArrayEqual = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     console.log(`😞😞Assertion Failed: ${arr1} !== ${arr2}`);
     return false;
@@ -13,8 +13,8 @@ let assertEqual = function(arr1, arr2) {
   return true;
 };
 
-console.log(assertEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertEqual([1, 2, 3], [1, 2, 3, 4]));
-console.log(assertEqual([1, 2, 3], [3, 2, 1, 4])); // => false
-console.log(assertEqual(['1', '2', '3'], ['1', '2', '3'])); // => true
-console.log(assertEqual(['1', '2', '3'], ['1', '2', 3])); // => false
+console.log(assertArrayEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArrayEqual([1, 2, 3], [1, 2, 3, 4]));
+console.log(assertArrayEqual([1, 2, 3], [3, 2, 1, 4])); // => false
+console.log(assertArrayEqual(['1', '2', '3'], ['1', '2', '3'])); // => true
+console.log(assertArrayEqual(['1', '2', '3'], ['1', '2', 3])); // => false
