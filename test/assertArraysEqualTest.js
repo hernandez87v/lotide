18da553fd22b16fa6d08f6db13b1438925833c11
Vlad <hernandez87v@hotmail.com>
2020-03-09 21:25:37 +0000

    Adding assertArraysEqual test

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+//const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 1, 4]); // => false
+assertArraysEqual(['1', '2', '3'], ['1', '2', '3']); // => true
+assertArraysEqual(['1', '2', '3'], ['1', '2', 3]); // => false
