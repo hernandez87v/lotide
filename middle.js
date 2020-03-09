f90d9f1a9dff38ad5bbd8ded825d6fe559087e7f
Vlad <hernandez87v@hotmail.com>
2020-03-09 21:35:39 +0000

    Adding middle test

--- middle.js
+++ middle.js
@@ -1,30 +1,3 @@
-let eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-let assertArrayEqual = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    console.log(`😞😞Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`😞😞Assertion Failed: ${arr1} !== ${arr2}`);
-      return false;
-    }
-  }
-  console.log(`😁😁Assertion Passed: ${arr1} === ${arr2}`);
-  return true;
-};
-
 const middle = function(array) {
   let midPoint = Math.floor(array.length / 2) - 1;
   if (array.length <= 2) {
@@ -36,8 +9,5 @@ const middle = function(array) {
     return [array[midPoint]];
   }
 };
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 4]));
-console.log(middle([1, 4, 5, 6]));
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+module.exports = middle;
