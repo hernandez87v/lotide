b38e2db8c589a336151cb27aed791b381f1676ac
Vlad <hernandez87v@hotmail.com>
2020-03-10 01:37:00 +0000

    Adding shortening of index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // index.js
-const head = require('./head');
-const tail = require('./tail');
-const middle = require('./middle');
+// const head = require('./head');
+// const tail = require('./tail');
+// const middle = require('./middle');
 
-module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+// module.exports = {
+//   head: head,
+//   tail: tail,
+//   middle: middle
+// };
+
+const module.exports = {
+    head: require('./head'),
+    tail: require('./tail'),
+    middle: require('./middle')
 };
+
+const { head, tail, middle } = module.exports;
