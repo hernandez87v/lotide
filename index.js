9e1cb9d483e2751a6c9d9ba6774e931eac4a1c12
Vlad <hernandez87v@hotmail.com>
2020-03-10 05:35:47 +0000

    Adding lotide changes

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 // index.js
-// const head = require('./head');
-// const tail = require('./tail');
-// const middle = require('./middle');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArrayEqual = require('./assertArraysEqual')
+const assertEqual = require('./assertEqual')
+const assertObjectEqual = require('./assertObjectEqual')
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly')
+const eqArrays = require('./eqArrays')
+const eqObjects = require('./eqObjects')
+const findKey = require('./findKey')
+const findKeyByValue = require('./findKeyByValue')
+const flatten = require('./flatten')
+const letterPositions = require('./letterPositions')
+const map = require('./map')
+const takeUntil = require('./takeUntil')
+const without = require('./without'),
 
-// module.exports = {
-//   head: head,
-//   tail: tail,
-//   middle: middle
-// };
 
-const module.exports = {
-    head: require('./head'),
-    tail: require('./tail'),
-    middle: require('./middle')
+module.exports = {
+    head,
+    tail,
+    middle,
+    assertArrayEqual,
+    assertEqual,
+    assertObjectEqual,
+    countLetters,
+    countOnly,
+    eqArrays,
+    eqObjects,
+    findKey,
+    findKeyByValue,
+    flatten,
+    letterPositions,
+    map,
+    takeUntil,
+    without
 };
-
-const { head, tail, middle } = module.exports;
